cf0c898b0b355521b617c74c99c1afdc56fd18c3
EricaXy <ericaxy15@gmail.com>
2023-10-26 19:12:23 +0800

    navigation

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    Header
+  </div>
+);
+
+export default Header;
